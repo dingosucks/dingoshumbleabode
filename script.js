251dab65eea46a681517a4e34bf29cc9b8ca2653
Glitch (glitch-hello-website) <none>
2025-02-10 19:51:29 +0000

    🐑👜 Checkpoint ./indexStyle.css:65991231/23 ./index.html:65991231/877 ./script.js:65991231/973

--- script.js
+++ script.js
@@ -30,23 +30,7 @@ var x = setInterval(function() {
 
 //photography changing wow!
 
-var pics = [
-  "https://cdn.glitch.global/8cc87ae8-2401-45b0-98ad-907c24699c04/c7b48853-0264-47fb-ac26-0bff96f7ede5.jfif?v=1736538712386",
-  "https://cdn.glitch.global/8cc87ae8-2401-45b0-98ad-907c24699c04/aeb38cae-0be8-46e4-882f-b5c1154c8d5e.jfif?v=1736538718752",
-  "https://cdn.glitch.global/8cc87ae8-2401-45b0-98ad-907c24699c04/77693718-d98e-4148-b574-2a1d250dac6a.jfif?v=1736538722343",
-  "https://cdn.glitch.global/8cc87ae8-2401-45b0-98ad-907c24699c04/594ff053-fee0-42f7-a1de-5dfe9e6dfbfb.jfif?v=1736538726165",
-    "https://cdn.glitch.global/8cc87ae8-2401-45b0-98ad-907c24699c04/6d0e86be-1acb-4996-933e-554172c9b484.jfif?v=1736538733813",
-  "https://cdn.glitch.global/8cc87ae8-2401-45b0-98ad-907c24699c04/6c9d8593-d122-49b7-b9ad-c3c279e4ba93.jfif?v=1736538738945",
-];
 
-var imgState = 0;
-
-var imgTag = document.getElementById("sauce");
-
-imgTag.addEventListener("click", function (event) {
-  imgState = ++imgState % pics.length;
-  event.target.src = pics[imgState];
-});
 
 
 
